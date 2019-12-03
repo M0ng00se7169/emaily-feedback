0040870808bca982e42386f347c9561453e7d2d2
M0ng00se7169 <NickKomarowski@gmail.com>
2019-12-03 16:29:45 +0100

    Fixed Vulnerabilities

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,6 @@
+export default [
+    { label: 'Survey Title', name: 'title', noValueError: 'You must provide a Survey Title' },
+    { label: 'Subject Line', name: 'subject', noValueError: 'You must provide a Subject Line' },
+    { label: 'Email body', name: 'body', noValueError: 'You must provide an Email' },
+    { label: 'Recipients List', name: 'recipients', noValueError: 'You must provide the list of Emails' }
+]
